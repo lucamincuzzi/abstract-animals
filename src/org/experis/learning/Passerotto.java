15cf390350a3fa8bfeeb82a35e38fcfcd8054015
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-04 15:00:47 +0200

    add: Passerotto class

---
+++ src/org/experis/learning/Passerotto.java
@@ -0,0 +1,17 @@
+package org.experis.learning;
+
+public class Passerotto extends Animale{
+    public Passerotto(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void verso() {
+        System.out.println("Cip cip!");
+    }
+
+    @Override
+    public void mangia() {
+        System.out.println();
+    }
+}
