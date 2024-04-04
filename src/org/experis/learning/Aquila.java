2890330367eb1bbf2f58d295d38dc9b29050cb38
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-04 15:08:48 +0200

    add: Aquila class

---
+++ src/org/experis/learning/Aquila.java
@@ -0,0 +1,17 @@
+package org.experis.learning;
+
+public class Aquila extends Animale{
+    public Aquila(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void verso() {
+        System.out.println("Screeeech!");
+    }
+
+    @Override
+    public void mangia() {
+        System.out.println("sta mangiando un topolino");
+    }
+}
